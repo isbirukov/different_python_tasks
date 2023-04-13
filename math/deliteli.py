e027298e8e7a8c94d04baec7532e23bdc3fede48
isbirukov <31694361+isbirukov@users.noreply.github.com>
2023-04-13 18:41:53 +0300

    Update deliteli.py
    НОД

--- math/deliteli.py
+++ math/deliteli.py
@@ -8,3 +8,15 @@ while i*i <= n:
         if i != n // i:
             a.append(n // i)
     i += 1
+
+"""
+НОД
+"""
+
+a = int(input('Введите первое число: '))
+b = int(input('Введите второе число: '))
+while b > 0:
+    c = a%b
+    a = b
+    b = c
+print(f'Нод={a}')
